35330b7dff2d777e1749452a814a93b45cf3ba7e
fer1tx <46248011+fer1tx@users.noreply.github.com>
2023-01-09 01:43:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import smtplib
+import socket
 
 def verify_email(email):
   # First, check if the email is in a valid format
@@ -11,15 +12,27 @@ def verify_email(email):
   try:
     # Use the python built-in 'socket' library to resolve the DNS of the domain
     socket.gethostbyname(domain)
-  except socket.error:
+  except socket.gaierror:
     return False
   
   # Finally, try to verify the email address using an SMTP server
   try:
-    smtp = smtplib.SMTP(f'smtp.{domain}')
-    smtp.verify(email)
+    # Use the python built-in 'smtplib' library to connect to the SMTP server
+    # of the domain and issue the 'VRFY' command to verify the email address
+    with smtplib.SMTP(f'smtp.{domain}') as smtp:
+      # Some SMTP servers may not support the VRFY command, in which case
+      # we'll use the 'RCPT TO' command instead.
+      try:
+        smtp.vrfy(email)
+      except smtplib.SMTPCommandError:
+        smtp.mail('')  # Send an empty 'MAIL FROM' command
+        code, message = smtp.rcpt(email)  # Send the 'RCPT TO' command
+        if (code // 100) == 2:  # 2xx status code indicates success
+          return True
+        else:
+          return False
     return True
-  except Exception:
+  except (smtplib.SMTPServerDisconnected, smtplib.SMTPException, socket.error):
     return False
 
 print(verify_email('example@gmail.com')) # True
